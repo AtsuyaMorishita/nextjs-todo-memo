7630260db776f58790a4a97e64c5bfbc5c87ce8f
AtsuyaMorishita <mori.a0426@gmail.com>
2023-04-16 16:52:01 +0900

    タスクの完了処理

--- src/components/TodoArea.tsx
+++ src/components/TodoArea.tsx
@@ -5,30 +5,50 @@ type TodoAreaType = {
   currentUser: any;
 };
 
+type resDataType = {
+  id: string;
+  todo: string;
+  isComplete: boolean;
+  date: string;
+};
+
 const TodoArea = ({ currentUser }: TodoAreaType) => {
   // if (!userInfo) throw new Error("ユーザー情報が取得できませんでした");
 
-  const [checked, isChecked] = useState(false); //チェッsクボックスの状態を管理
+  const [isChecked, setsChecked] = useState(false); //チェックボックスの状態を管理
   const [inputText, setInputText] = useState(""); //タスクをテキストを管理
   const [remainingTasks, setRemainingTasks] = useState([]); //残タスクを管理
-  // const [finishTasks, setFinishTasks] = useState(userInfo["completed-tasks"]); //完了タスクを管理
+  const [completeTasks, setCompleteTasks] = useState([]); //完了タスクを管理
 
   //チェック時のスタイル
   const activeStyle = `after:absolute after:w-full after:h-[1px] after:left-0 after:top-[50%] after:bg-main`;
-  const checkedStyle = checked ? activeStyle : "";
+  const checkedStyle = isChecked ? activeStyle : "";
 
   useEffect(() => {
-    /**
-     * 全てのタスクを取得する
-     */
-    const getTask = async () => {
-      const resData = await axios.post("/api/getTask", {
-        currentUserId: currentUser.uid,
-      });
-      setRemainingTasks(resData.data);
-    };
     getTask();
   }, []);
+
+  /**
+   * 全てのタスクを取得する
+   */
+  const getTask = async () => {
+    //全てのtodoを取得
+    const resData = await axios.post("/api/getTask", {
+      currentUserId: currentUser.uid,
+    });
+
+    //残タスクだけ絞り込み
+    const resRemainingTasks = resData.data.filter((task: resDataType) => {
+      return task.isComplete === false;
+    });
+    setRemainingTasks(resRemainingTasks);
+
+    //完了タスクだけ絞り込み
+    const resCompleteTasks = resData.data.filter((task: resDataType) => {
+      return task.isComplete === true;
+    });
+    setCompleteTasks(resCompleteTasks);
+  };
 
   /**
    * タスクを追加する
@@ -37,17 +57,43 @@ const TodoArea = ({ currentUser }: TodoAreaType) => {
     //追加するデータ
     const todoData = {
       date: "4月1日",
-      isComplate: false,
+      isComplete: false,
       todo: inputText,
     };
 
     try {
+      //全てのtodoを取得
       const resData = await axios.post("/api/addTask", {
         currentUserId: currentUser.uid,
         todoData,
       });
-      setRemainingTasks(resData.data);
+
+      //残タスクだけ絞り込み
+      const resRemainingTasks = resData.data.filter((task: resDataType) => {
+        return task.isComplete === false;
+      });
+
+      setRemainingTasks(resRemainingTasks);
       setInputText("");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  /**
+   * 残タスクから完了タスクへの移動
+   */
+  const handleRemained = async (inputElement: any) => {
+    try {
+      //残タスクの状態を完了に変更
+      const checkTaskId = await inputElement.target.id;
+      await axios.put("/api/finishTask", {
+        currentUserId: currentUser.uid,
+        checkTaskId,
+      });
+
+      //全てのTodoを取得して画面に再描写
+      getTask();
     } catch (err) {
       console.log(err);
     }
@@ -88,16 +134,6 @@ const TodoArea = ({ currentUser }: TodoAreaType) => {
       // );
       // setUserTasks(newUserInfo.data[0]["remaining-tasks"]);
       // setFinishTasks(newUserInfo.data[0]["completed-tasks"]);
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
-  //残タスクのチェックボックスをクリック時の処理
-  const handleRemained = async (inputElement: any) => {
-    try {
-      // await finishTask(inputElement.target.value);
-      // inputElement.target.checked = false;
     } catch (err) {
       console.log(err);
     }
@@ -159,11 +195,11 @@ const TodoArea = ({ currentUser }: TodoAreaType) => {
           {remainingTasks.map((task: any, index) => (
             <li className="my-3" key={index}>
               <label
-                htmlFor={`task_${task.id}`}
+                htmlFor={`${task.id}`}
                 className="flex items-center cursor-pointer"
               >
                 <input
-                  id={`task_${task.id}`}
+                  id={`${task.id}`}
                   type="checkbox"
                   className="cursor-pointer w-[20px] h-[20px]"
                   value={task.todo}
@@ -182,23 +218,23 @@ const TodoArea = ({ currentUser }: TodoAreaType) => {
           完了タスク
         </p>
         <ul className="mt-6">
-          {/* {finishTasks.map((task, index) => (
+          {completeTasks.map((task: any, index) => (
             <li className="my-3" key={index}>
               <label
-                htmlFor={`finishTask_${index}`}
+                htmlFor={`finishTask_${task.id}`}
                 className="flex items-center cursor-pointer"
               >
                 <input
-                  id={`finishTask_${index}`}
+                  id={`finishTask_${task.id}`}
                   type="checkbox"
                   className="cursor-pointer w-[20px] h-[20px]"
                   checked
                   onChange={() => handleCompleted(task)}
                 />
-                <p className={`ml-2 relative ${activeStyle}`}>{task}</p>
+                <p className={`ml-2 relative ${activeStyle}`}>{task.todo}</p>
               </label>
             </li>
-          ))} */}
+          ))}
         </ul>
         <div className="text-right">
           <button
