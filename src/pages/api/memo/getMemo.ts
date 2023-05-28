af1f93a018f7e2e4e036fc0ec31d2f4ee232371d
AtsuyaMorishita <mori.a0426@gmail.com>
2023-05-28 16:47:51 +0900

    メモを追加順に並び替える

--- src/pages/api/memo/getMemo.ts
+++ src/pages/api/memo/getMemo.ts
@@ -1,4 +1,4 @@
-import { collection, getDocs } from "firebase/firestore";
+import { collection, getDocs, orderBy, query } from "firebase/firestore";
 import type { NextApiRequest, NextApiResponse } from "next";
 import { db } from "../../../../lib/firebase";
 
@@ -6,6 +6,7 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
   const currentUserId = req.body.currentUserId;
 
   const usersCollectionRef = collection(db, "user", currentUserId, "memo");
+  const q = query(usersCollectionRef, orderBy("timestamp", "desc"));
 
   let allMemos: any = [];
 
@@ -14,7 +15,7 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
       /**
        * 全てのメモを取得する
        */
-      await getDocs(usersCollectionRef).then((snapshot) => {
+      await getDocs(q).then((snapshot) => {
         snapshot.docs.forEach((doc) => {
           allMemos.push({ id: doc.id, ...doc.data() });
         });
